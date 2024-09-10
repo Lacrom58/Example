2cb9b3582b3582b5b4edab0a0ab17b2e95626e84
Alex Balakin <b_aleksandr_a@mail.ru>
2024-09-10 20:54:25 +0300

    Удалил отступы

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by Сергей Устинов on 10.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
