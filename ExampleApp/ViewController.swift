6d8d7e8356f7ba7c68bd1088d920ff73bbd7b963
Alex Balakin <b_aleksandr_a@mail.ru>
2024-09-10 22:26:11 +0300

    add print fullName

--- ExampleApp/ViewController.swift
+++ ExampleApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let name = "Sergey Ustinov"
+    let name = "Sergey"
+    let secondName = "Ustinov"
+    
+    var fullName: String {
+        name + secondName
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("View did load")
+        print(fullName)
     }
 }
 
